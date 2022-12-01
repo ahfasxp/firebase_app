5325500515011e8f4a1a4c5c24c155e430631197
ahfasxp1 <ahfas.fazria@gmail.com>
2022-12-01 09:24:44 +0700

    dynamic link android

---
+++ lib/services/dynamic_links_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:flutter/foundation.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class DynamicLinksService {
+  static Future<void> initDynamicLinks() async {
+    // NOTE: Listen for terminated state
+    // Get any initial links
+    final PendingDynamicLinkData? _initialLink =
+        await FirebaseDynamicLinks.instance.getInitialLink();
+    if (_initialLink != null) {
+      _handleDynamicLink(_initialLink);
+    }
+
+    // NOTE: Listen for background / foreground state
+    FirebaseDynamicLinks.instance.onLink.listen((dynamicLink) {
+      _handleDynamicLink(dynamicLink);
+    }).onError((error) {
+      debugPrint(error);
+    });
+  }
+
+  static void _handleDynamicLink(PendingDynamicLinkData dynamicLink) async {
+    final Uri _deepLink = dynamicLink.link;
+
+    debugPrint(_deepLink.toString());
+    Fluttertoast.showToast(
+      msg: _deepLink.toString(),
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      fontSize: 16.0,
+    );
+  }
+}
